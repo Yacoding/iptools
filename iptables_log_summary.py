67ece748b3ed0dfb59ee35e12c59ef462035dc1a
Michael Haskell <mikehas@gmail.com>
2014-10-13 12:42:13 -0700

    iptables_log_summary.py - added date stamps of most recent activity, and recording of multicast traffic.

--- iptables_log_summary.py
+++ iptables_log_summary.py
@@ -3,34 +3,40 @@
 
 '''
 Find the number of dropped connection messages from remote machines to local ports
-  built to check /var/log/messages to assist in establishing iptables rules.
 '''
 import sys
 import glob
 import gzip
 import socket
+import datetime
+import time
 
 print "\nBeginning analysis of /var/log/messages"
 lfile = '/var/log/messages'
 
-files = glob.glob("/var/log/messages*") 
+files = glob.glob("/var/log/messages*")
+
+now = datetime.datetime.now()
 
 events = []
 
 def parseline(line,start):
-  event= {}
-  try:
-    for item in line[start:-1]:
-      item = item.split("=")
-      key = item[0]
-      if len(item) > 1:
-        val = item[1]  
-      else: 
-        val = '';
-      event[key] = val
-    return event
-  except:
-    pass
+    event= {}
+    try:
+      for item in line[start:-1]:
+        item = item.split("=")
+        key = item[0]
+        if len(item) > 1:
+          val = item[1]
+        else:
+          val = '';
+        event[key] = val
+      event["month"] = line[0]
+      event["day"] = line[1]
+      event["time"] = line[2]
+      return event
+    except:
+      pass
 
 def registerevent(event):
   events.append(event)
@@ -42,32 +48,40 @@ def summarizeevents():
     try:
       origip = e['SRC']
       ip = origip.replace(".","_")
-      port = e['DPT']
+      port = "MULTI"
+      if 'DPT' in e:
+        port = e['DPT']
+
+      date = time.strptime(str(now.year) + " " + str(e["month"]) + " " + str(e["day"].zfill(2)) + " "+ str(e["time"]), "%Y %b %d %H:%M:%S")
+
       knownconn = False
       if port not in uniqueports:
         uniqueports.append(port)
       if ip in conns:
         if port in conns[ip]:
           conns[ip][port] += 1
+          if (conns[ip]["date"] < date):
+            conns[ip]["date"] = date
         else:
           conns[ip][port] = 1
       else:
-        hostname = " - "
+        hostname = "NXDOMAIN"
         try:
           hostname = socket.gethostbyaddr(origip)[0]
         except:
           pass
-        conns[ip] = {port: 1, "hostname": hostname}
-    except:
+        conns[ip] = {port: 1, "hostname": hostname, "date" : date}
+    except Exception, myerr:
+      #sys.stdout.write( str(myerr))
       pass
 
   # Print the machine and dest port and the number of times.
-  
-  print "\nDROP Log Messages\nSource IP\tPort\tCount\tHostname"
+
+  print "\nDROP Log Messages\nLast Connection Date\t Source IP\tPort\tCount\tHostname"
   for ip in conns:
     for port in conns[ip]:
-      if port != "hostname":
-        print ip + "\t" + port + "\t" + str(conns[ip][port]) + "\t" + conns[ip]["hostname"] 
+      if port != "hostname" and port != "date": #terrible exclusion
+        print time.strftime("%x %X", conns[ip]["date"]) + "\t" + ip.replace('_','.') + "\t" + port + "\t" + str(conns[ip][port]) + "\t" + conns[ip]["hostname"]
   print "\nUnique Destination Ports"
   for p in uniqueports:
     print p
@@ -83,12 +97,13 @@ for file in files:
   for line in logs:
     l =  line.split()
     type = l[5]
+    #sys.stdout.write(type + " ");
     start = 0
     if type == "IPTABLES":
       start = 6
-    elif type == "IPTABLES-DROP":
-      start = 7
+    elif type == "IPTABLES-DROP:":
+      start = 6
     event = parseline(l,start)
     registerevent(event)
-  
+
 summarizeevents()
